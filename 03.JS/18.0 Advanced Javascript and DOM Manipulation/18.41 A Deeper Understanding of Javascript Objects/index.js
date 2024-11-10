f5fe7291cf47277e96498d60389623511422cc5d
claudiotassis <claudiotassis@gmail.com>
2024-11-10 15:41:02 -0300

    updated course progress

---
+++ 03.JS/18.0 Advanced Javascript and DOM Manipulation/18.41 A Deeper Understanding of Javascript Objects/index.js
@@ -0,0 +1,9 @@
+function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function () {
+        alert("Cleaning in progress...");
+    }
+}
+//var houseKeeper1 = new HouseKeeper (12, "Jane", ["bathroom", "lobby", "bedroom"]);
